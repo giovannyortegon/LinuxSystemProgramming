974ddf73d8609ecc2a75cf92027d7742b12cc5fb
giovannyortegon <ortegon.giovanny@hotmail.com>
2021-11-16 22:14:46 -0500

    [ADD] using macro errno.h

---
+++ C/Chap05/huge-test.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(void)
+{
+	int number = 9999;
+	double answer;
+
+	if ((answer = pow(number, number)) == HUGE_VAL)
+	{
+		fprintf(stderr, "A huge value\n");
+		return (1);
+	}
+	else
+	{
+		printf("%lf\n", answer);
+	}
+
+	return (1);
+}
