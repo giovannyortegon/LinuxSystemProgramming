32ea80c0b642580a34773adaef1369297451d4e0
giovannyortegon <ortegon.giovanny@hotmail.com>
2022-01-13 21:29:44 -0500

    [ADD] handle error

---
+++ GO/src/Chap03/funErr.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"errors"
+	"log"
+)
+
+func division(x int, y int) (int, error, error) {
+	if y == 0 {
+		return 0, nil, errors.New("Can not divide by zero!")
+	}
+
+	if x % y != 0 {
+		remainder := errors.New("There is a remainder!")
+		return x / y, remainder, nil
+	} else {
+		return x / y, nil, nil
+	}
+}
+
+func main() {
+	result, rem, err := division(2, 2)
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("The result is", result)
+	}
+
+	if rem != nil {
+		fmt.Println(rem)
+	}
+
+	result, rem, err = division(12, 5)
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("The result is", result)
+	}
+
+	if rem != nil {
+		fmt.Println(rem)
+	}
+
+	result, rem, err = division(2, 0)
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("The result is", result)
+	}
+
+	if rem != nil {
+		fmt.Println(rem)
+	}
+}
