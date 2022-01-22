942d9d670b78b05eac2b4e9d182a9a52fb787af1
giovannyortegon <ortegon.giovanny@hotmail.com>
2022-01-22 01:12:12 -0500

    [ADD] login, index functions

--- GO/src/WebChap02/main.go
+++ GO/src/WebChap02/main.go
@@ -1,13 +1,27 @@
 package main
 
 import "net/http"
+import "chitchat/"
 
 func index(w http.ResponseWriter, r * http.Request) {
-	files := []string{"templates/layout.html",
-					  "templates/navbar.html",
-					  "templates/index.html",}
-	templates := template.Must(template.ParseFiles(files...))
 	threads, err := data.Threads(); if err == nil {
+		_, err := session(w, r)
+		public_tmpl_files := []string{"templates/layout.html",
+									  "templates/navbar.html",
+									  "templates/index.html",}
+		private_tmpl_files := []string{"templates/layout.html",
+									  "templates/navbar.html",
+									  "templates/index.html",}
+
+		var templates * template.Template
+
+		if err != nil {
+			templates := template.Must(
+						template.ParseFiles(private_tmpl_files...))
+		} else {
+			templates := template.Must(
+						template.ParseFiles(public_tmpl_files...))
+		}
 		templates.ExecuteTemplate(w, "layout", threads)
 	}
 }
---
+++ GO/src/WebChap02/util.go
@@ -0,0 +1,15 @@
+package utils
+
+func session(w http.ResponseWriter,
+			 r * http.Request) (sess data.Session, err error){
+	cookie, err := r.Cookie("_cookie")
+
+	if err == nil {
+		sess = data.Session{ Uuid: cookie.Value }
+		if ok, _ := sess.Check(); !ok {
+			err = errors.New("Invalid session")
+		}
+	}
+
+	return
+}
