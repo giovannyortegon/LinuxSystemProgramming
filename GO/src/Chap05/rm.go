6c11334c16378dfbfca7cffb656e15e1fd50a48f
giovannyortegon <ortegon.giovanny@hotmail.com>
2022-02-07 07:48:09 -0500

    [ADD] creating rm and rename with go

---
+++ GO/src/Chap05/rm.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+import "os"
+
+func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide an argument!")
+		os.Exit(1)
+	}
+
+	file := arguments[1]
+	err := os.Remove(file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
