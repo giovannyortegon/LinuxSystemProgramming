12d6c2ae1f25772ef65fa8fc10b102c236dfe3a5
giovannyortegon <ortegon.giovanny@hotmail.com>
2022-02-09 08:26:46 -0500

    [ADD] move

---
+++ CPP/Chap02/use_move01.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+
+int main() {
+	std::vector<int> a = {1, 2, 3, 4, 5};
+	auto b = std::move(a);
+
+	std::cout <<"a: " <<a.size() <<std::endl;
+	std::cout <<"b: " <<b.size() <<std::endl;
+
+	return (0);
+}
---
+++ CPP/Chap02/use_move02.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+
+void print (std::string && s);
+void print(std::string &s);
+
+int main() {
+	std::string str ("this is a string");
+	print(str);
+	std::cout <<" ==> str = " <<str <<std::endl;
+
+	return (0);
+}
+void print (std::string && s) {
+	std::cout <<"print(std::string &&s)" <<std::endl;
+	std::string str (std::move(s));
+	std::cout <<"universal reference ==> str = " <<str <<std::endl;
+	std::cout <<"universal reference ==> s = " <<s <<std::endl;
+}
+
+void print(std::string &s) {
+	std::cout <<"print(std::string &s) " <<std::endl;
+}
