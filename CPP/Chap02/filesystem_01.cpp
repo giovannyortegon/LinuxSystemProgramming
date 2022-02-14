e7cba23bac1c9f25d4e4a1370ae5829e2eac5225
giovannyortegon <ortegon.giovanny@hotmail.com>
2022-02-14 07:45:45 -0500

    [ADD] concurrencies

---
+++ CPP/Chap02/filesystem_01.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<filesystem>
+
+int main() {
+	for (auto& p : std::filesystem::directory_iterator("/"))
+	std::cout <<p << std::endl;
+
+	return (0);
+}
---
+++ CPP/Chap02/filesystem_02.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<filesystem>
+#include<fstream>
+
+int main() {
+	std::filesystem::create_directories("test/src/config");
+	std::ofstream("test/src/file.txt") <<"This is an example!";
+
+	return (0);
+}
