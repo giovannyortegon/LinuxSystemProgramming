852b8f7787ff14b3f5448893a36f1dbd06747745
giovannyortegon <ortegon.giovanny@hotmail.com>
2022-05-22 10:06:26 -0500

    [ADD] adding variable and function

--- DART/Sesion01/hello.dart
+++ DART/Sesion01/hello.dart
@@ -1,3 +1,13 @@
-void main () {
+/* First Program */
+
+main () {
   print("Hello, World!");
+
+  // Declara variable
+  var number = 2;
+
+  PrintVariable(number);
+}
+PrintVariable(int number) {
+  print("Valor numero: $number");
 }
