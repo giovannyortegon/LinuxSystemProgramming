d3ad69f6c8c09eaf3695cac7b1777ce2c5b45157
giovannyortegon <ortegon.giovanny@hotmail.com>
2022-07-13 22:30:51 -0500

    [ADD] desplazamiento array 2D

--- C/Pointers/Chap05/MultiDim4.c
+++ C/Pointers/Chap05/MultiDim4.c
@@ -4,6 +4,7 @@ int main() {
     int data[5][5];
     int i, j;
     int count = 0;
+    int (*aptr)[5];
 
     for (i = 0; i < 5; i++) {
         for (j = 0; j < 5; j++) {
@@ -11,13 +12,10 @@ int main() {
         }
     }
 
-    printf("Start address of the array %p\n", data);
+    aptr = data;
+
     for (i = 0; i < 5; i++) {
-        printf(" %dth row = %p\n", i, data[i]);
-        printf("Columns\n");
-        for (j = 0; j < 5; j++)
-            printf("%d = %p, ", j, data[i]+j);
-        printf("\n");
+        printf("%dth row  = %p\n", i, *aptr++);
     }
 
     return (0);
