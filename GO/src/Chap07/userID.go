7c8abbef77290620e12e73025761d8e9512d58c2
giovannyortegon <ortegon.giovanny@hotmail.com>
2022-08-12 17:27:44 -0500

    [ADD] get users

---
+++ GO/src/Chap07/userID.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+)
+
+func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		uid := os.Getegid()
+		fmt.Println(uid)
+
+		return
+	}
+
+	username := arguments[1]
+	u, err := user.Lookup(username)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(u.Uid)
+
+}
