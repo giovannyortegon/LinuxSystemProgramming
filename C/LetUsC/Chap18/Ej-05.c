f3d466bb6749bbde61eb1744fb01f6869c2e3a58
Giovanny Ortegon <37087716+giovannyortegon@users.noreply.github.com>
2022-10-06 23:40:11 -0500

    [ADD] drawing with mouse

--- C/LetUsC/Chap18/Ej-05.c
+++ C/LetUsC/Chap18/Ej-05.c
@@ -1,11 +1,13 @@
 #include<Windows.h>
 #include"helper.h"
 
+int x1, y_1, x2, y2;
+
 void OnLButtonDown(HWND, int, int);
+void OnLButtonUp();
+void OnMouseMove(HWND, int , int, int);
 void OnDestroy(HWND);
 BOOL InitInstance(HINSTANCE, int, char *);
-
-int x1, y1, x2, y2;
 
 int _stdcall WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdSHow) {
     MSG m;
@@ -25,7 +27,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM Lparam) 
             OnDestroy(hWnd);
             break;
         case WM_LBUTTONDOWN:
-            OnLButtonDown(hWnd, LOW);
+            OnLButtonDown(hWnd, LOWORD(Lparam), HIWORD(Lparam));
+            break;
+        case WM_LBUTTONUP:
+            OnLButtonUp();
+            break;
+        case WM_MOUSEMOVE:
+            OnMouseMove(hWnd, wParam, LOWORD(Lparam), HIWORD(Lparam));
             break;
         default:
             return DefWindowProc(hWnd, message, wParam, Lparam);
@@ -38,6 +46,27 @@ void OnDestroy(HWND hWnd) {
     PostQuitMessage(0);
 }
 
-void OnLButtonDown(HWND, int, int) {
+void OnLButtonDown(HWND hWnd, int x, int y) {
+    SetCapture(hWnd);
+    x1 = x;
+    y_1 = y;
+}
 
+void OnLButtonUp() {
+    ReleaseCapture();
+}
+void OnMouseMove(HWND hWnd, int flags, int x, int y) {
+    HDC hdc;
+
+    if (flags == MK_LBUTTON) {
+        hdc = GetDC(hWnd);
+        x2 = x;
+        y2 = y;
+        MoveToEx(hdc, x1, y_1, NULL);
+        LineTo(hdc, x2, y2);
+        ReleaseDC(hWnd, hdc);
+
+        x1 = x2;
+        y_1 = y2;
+    }
 }
